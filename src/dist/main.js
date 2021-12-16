631dc63a97e42f9edcc71a89ac690ee810dcb22a
Fanbayy <fandi.bayu110@gmail.com>
2021-12-16 17:43:46 +0700

    solved scroll reveal js and delete spin waiting

--- src/dist/main.js
+++ src/dist/main.js
@@ -1,3 +1,18 @@
+// scroll reveral js
+const sr = ScrollReveal({
+  origin: "top",
+  duration: 1500,
+  distance: "20px",
+  reset: true,
+});
+
+sr.reveal(`.anotherProject, #project, #contact, .certificateHeading, #home`);
+sr.reveal(`.certificate1, .certificate2, .certificate3`, { interval: 150 });
+sr.reveal(`.certificate4, .certificate5, .certificate6`, { interval: 250 });
+sr.reveal(`.about-text, .redesign-2`, { origin: `right` });
+sr.reveal(`.about-img, .redesign-1`, { origin: `left` });
+sr.reveal(`.redesign-3`, { origin: `bottom` });
+
 $(document).ready(function () {
   $(".nav-toggler").each(function (_, navToggler) {
     var target = $(navToggler).data("target");
@@ -9,28 +24,13 @@ $(document).ready(function () {
   });
 });
 
-// loader
-var myVar;
-function myFunction() {
-  myVar = setTimeout(showPage, 100);
-}
+// // loader
+// var myVar;
+// function myFunction() {
+//   myVar = setTimeout(showPage, 100);
+// }
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
-
-// scroll reveral js
-const sr = ScrollReveal({
-  origin: "top",
-  duration: 1500,
-  distance: "20px",
-  reset: true,
-});
-
-sr.reveal(`.anotherProject, #project, #contact, .certificateHeading`);
-sr.reveal(`.certificate1, .certificate2, .certificate3`, { interval: 100 });
-sr.reveal(`.certificate4, .certificate5, .certificate6`, { interval: 150 });
-sr.reveal(`.about-text, .redesign-2`, { origin: `right` });
-sr.reveal(`.about-img, .redesign-1`, { origin: `left` });
-sr.reveal(`.redesign-3`, { origin: `bottom` });
+// function showPage() {
+//   document.getElementById("loader").style.display = "none";
+//   document.getElementById("myDiv").style.display = "block";
+// }
